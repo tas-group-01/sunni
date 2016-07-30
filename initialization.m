c109c476cbd6115bd5bed9ec9c73bf9a7aea7da4
Thomas Markowitz <markowitz.thomas@gmail.com>
2016-07-30 11:24:17 +0200

    sampling reduktion

--- initialization.m
+++ initialization.m
@@ -5,6 +5,8 @@ features=extractHOGFeatures(screencapture(0, [385,155,125,55]), 'CellSize', [4 4
 label = predict(CLASSIFIER_Initialization, features); 
 
 while(label == 1)
+    
+    pause(3); %reduktion sampling rate
 
     features=extractHOGFeatures(screencapture(0, [385,155,125,55]), 'CellSize', [4 4]);
     label = predict(CLASSIFIER_Initialization, features);
