c546c238af4e824988c118cb7daa1f60cc26c492
Thomas Markowitz <markowitz.thomas@gmail.com>
2016-07-30 19:41:22 +0200

    players involved

---
+++ involved.m
@@ -0,0 +1,15 @@
+function N = involved()
+%% count players 
+global CLASSIFIER_Plays;
+
+filter = [-2 -2 -2;-2 14 -2;-2 -2 -2];
+I1 = imfilter(rgb_to_gray(screencapture(0, [210,295,30,45])),filter);
+I2 = imfilter(rgb_to_gray(screencapture(0, [215,610,30,45])),filter);
+I3 = imfilter(rgb_to_gray(flip(screencapture(0, [545,610,30,45]),2)),filter);
+
+N = not(predict(CLASSIFIER_Plays,extractHOGFeatures(I1,'CellSize',[4 4])))+not(predict(CLASSIFIER_Plays,extractHOGFeatures(I2,'CellSize',[4 4])))+not(predict(CLASSIFIER_Plays,extractHOGFeatures(I3,'CellSize',[4 4])));
+
+
+
+
+end
