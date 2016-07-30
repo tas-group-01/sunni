4574bd04c0811d0e68327d9a99419ed10240ca0a
Thomas Markowitz <markowitz.thomas@gmail.com>
2016-07-30 17:25:26 +0200

    values < 100 inserted

--- pot_size.m
+++ pot_size.m
@@ -15,9 +15,13 @@ for i=1:14 %find the $
         if ref < 6
             %pot value > 1000
             pot_4_digits = 1;
-        else
+        elseif ref < 12
             %pot value < 1000
             pot_4_digits = 0;
+        elseif ref < 15
+            %pot value < 100
+            pot_4_digits = -1;
+            
         end
         
     end            
@@ -50,6 +54,17 @@ switch pot_4_digits
         %transform to number
         n = [digit_1 digit_2 digit_3 digit_4];
         pot = n*10.^(numel(n)-1:-1:0).';
+    case -1
+        %classify the first, second for pot value < 100
+        first = I(:,ref+5:ref+11);
+        second = I(:,ref+16:ref+22);        
+        %perform classification
+        digit_1 = predict(CLASSIFIER_Pot, extractHOGFeatures(first, 'CellSize', [2 2])); 
+        digit_2 = predict(CLASSIFIER_Pot, extractHOGFeatures(second, 'CellSize', [2 2])); 
+        %transform to number
+        n = [digit_1 digit_2];
+        pot = n*10.^(numel(n)-1:-1:0).';        
+        
     case 8
         msg = 'error occured in classifying the pot';
         warning(msg);
