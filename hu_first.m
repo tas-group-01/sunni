ac07e0fa2b7b47b5b24c187a6b60efcac1d1b199
Thomas Markowitz <thomas.markowitz@inopus.local>
2016-08-05 09:38:37 +0200

    Deleted

--- hu_first.m
+++
@@ -1,9 +0,0 @@
-function Y = hu_first(bt_hu_classifierObs)
-
-bt = screencapture(0, [565,406,25,25]);
-bt_gray = rgb_to_gray(bt);
-bt_there = extractHOGFeatures(bt_gray,'CellSize',[4 4]);
-Y = predict(bt_hu_classifierObs,bt_there);
-
-
-end
