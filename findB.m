9fe8f31365f9534c832dee3b69a147c22fa06aef
Thomas Markowitz <markowitz.thomas@gmail.com>
2016-09-11 11:08:24 +0200

    "made find Button more robust with filtering operation"

--- findB.m
+++ findB.m
@@ -3,18 +3,21 @@ function Button = findB()
 global CLASSIFIER_Button;
 %global game_duration;
 
+% introducing filter for robustness
+filter = [-2 -2 -2;-2 14 -2;-2 -2 -2];
+
 %prediction
-B1 = predict(CLASSIFIER_Button, extractHOGFeatures(rgb_to_gray(screencapture(0, [310,340,25,25])),'CellSize', [4 4]));
-B2 = predict(CLASSIFIER_Button, extractHOGFeatures(rgb_to_gray(screencapture(0, [220,570,25,25])),'CellSize', [4 4]));
-B3 = predict(CLASSIFIER_Button, extractHOGFeatures(rgb_to_gray(screencapture(0, [475,575,25,25])),'CellSize', [4 4]));
-B0 = predict(CLASSIFIER_Button, extractHOGFeatures(rgb_to_gray(screencapture(0, [565,350,25,25])),'CellSize', [4 4]));
+B1 = predict(CLASSIFIER_Button, extractHOGFeatures(imfilter(rgb_to_gray(screencapture(0, [310,340,25,25])),filter),'CellSize', [4 4]));
+B2 = predict(CLASSIFIER_Button, extractHOGFeatures(imfilter(rgb_to_gray(screencapture(0, [220,570,25,25])),filter),'CellSize', [4 4]));
+B3 = predict(CLASSIFIER_Button, extractHOGFeatures(imfilter(rgb_to_gray(screencapture(0, [475,575,25,25])),filter),'CellSize', [4 4]));
+B0 = predict(CLASSIFIER_Button, extractHOGFeatures(imfilter(rgb_to_gray(screencapture(0, [565,350,25,25])),filter),'CellSize', [4 4]));
 
 B = [B0 B1 B2 B3];
-if sum(not(B)) > 1
+if sum((B)) > 1
     msg = 'button classification failed 1.0';
     warning(msg);
 end
-Button = find(B == 0);
+Button = find(B ~= 0);
 if length(Button) > 1
     msg = 'button classification failed 1.1';
     warning(msg);
